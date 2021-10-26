cdd0fb700634250f607d6966dc0ae08116de43f7
Rachel Cianfichi <cianfich@gmail.com>
2021-10-26 17:13:46 -0400

    Updated controller routes for comments, posts, and users.

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const { Post } = require('../../models/');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  
+    try {
+      const newPost = await Post.create({ ...req.body, userId: req.session.userId });
+      res.json(newPost);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  router.put('/:id', withAuth, async (req, res) => {
+    try {
+      const updatedPost = await Post.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (!updatedPost) {
+        res.status(404).json({ message: 'No post found with this specific id.' });
+        return;
+      }
+  
+      res.status(200).json(updatedPost);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  router.delete('/:id', withAuth, async (req, res) => {
+    try {
+      const deletedPost = Post.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (!deletedPost) {
+        res.status(404).json({ message: 'No post found with this specific id.' });
+        return;
+      }
+  
+      res.status(200).json(deletedPost);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,22 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+router.post('/', async (req, res) => {
+  try {
+    const userInfo = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userInfo.id;
+      req.session.username = userInfo.username;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 router.post('/login', async (req, res) => {
     try {
       const userData = await User.findOne({ where: { username: req.body.username } });
